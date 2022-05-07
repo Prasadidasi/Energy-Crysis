241850a11c5b30c0a289d3d1f931b063a9798f18
Korrasami1 <tash123@live.co.za>
2022-05-07 19:36:54 +0200

    added vfx instead to light

--- Assets/Scripts/Lights.cs
+++ Assets/Scripts/Lights.cs
@@ -48,7 +48,6 @@ public class Lights : MonoBehaviour
         currentEnergyTime = EnergyTime;
         LightsOffPrefab.SetActive(false);
         this.GetComponent<SpriteRenderer>().sprite = LightOn;
-        this.GetComponent<SpriteRenderer>().sortingOrder = 0;
         UpdateEnergy();     
     }
 
@@ -56,7 +55,6 @@ public class Lights : MonoBehaviour
     {
         isLightsOn = false;
         this.GetComponent<SpriteRenderer>().sprite = LightOff;
-        this.GetComponent<SpriteRenderer>().sortingOrder = 1;
         currentEnergyTime = EnergyTime;
         LightsOffPrefab.SetActive(true);
     }
