2f756818d753b88f36bf2703069f55ec1c9703b7
Korrasami1 <tash123@live.co.za>
2022-05-08 11:13:33 +0200

    added rest of sfx in game. gameOver+hunger is there too but not playing

--- Assets/Scripts/ScenePanning.cs
+++ Assets/Scripts/ScenePanning.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class ScenePanning : MonoBehaviour
 {
     private Camera mainCamera;
+    AudioManager audioManager;
 
     [Header("Camera")]
     public float panAmount = 500f;
@@ -13,10 +14,13 @@ public class ScenePanning : MonoBehaviour
     public float SceneWidth = 1080f;
     public float SceneHeight = 1920f;
 
+    [Header("SFX")]
+    public string sfxName;
 
     private void Awake()
     {
         mainCamera = GameObject.FindObjectOfType<Camera>();
+        audioManager = GameObject.FindObjectOfType<AudioManager>();
         if(mainCamera == null)
         {
             Debug.Log("main camera not found");
@@ -30,6 +34,7 @@ public class ScenePanning : MonoBehaviour
 
     public void PanLeft()
     {
+        audioManager.Play(sfxName);
         if((mainCamera.transform.position.x) > 0)
         {
             mainCamera.transform.position = new Vector3(mainCamera.transform.position.x - panAmount, mainCamera.transform.position.y, mainCamera.transform.position.z);
@@ -40,6 +45,7 @@ public class ScenePanning : MonoBehaviour
     // Update is called once per frame
     public void PanRight()
     {
+        audioManager.Play(sfxName);
         if ((mainCamera.transform.position.x ) < SceneWidth)
         {
             mainCamera.transform.position = new Vector3(mainCamera.transform.position.x + panAmount, mainCamera.transform.position.y, mainCamera.transform.position.z);
@@ -48,6 +54,7 @@ public class ScenePanning : MonoBehaviour
 
     public void PanUp()
     {
+        audioManager.Play(sfxName);
         if ((mainCamera.transform.position.y) < SceneHeight)
         {
             mainCamera.transform.position = new Vector3(mainCamera.transform.position.x, mainCamera.transform.position.y + panAmount, mainCamera.transform.position.z);
@@ -56,6 +63,7 @@ public class ScenePanning : MonoBehaviour
 
     public void PanDown()
     {
+        audioManager.Play(sfxName);
         if ((mainCamera.transform.position.y) > -SceneHeight)
         {
             mainCamera.transform.position = new Vector3(mainCamera.transform.position.x, mainCamera.transform.position.y - panAmount, mainCamera.transform.position.z);
